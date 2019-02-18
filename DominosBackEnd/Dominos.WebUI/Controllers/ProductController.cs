8de94ce07bbe190c28e11750a02de6893e4a38ca
alicanblnr <47721034+alicanblnr@users.noreply.github.com>
2019-02-18 22:36:22 +0300

    Final

--- DominosBackEnd/Dominos.WebUI/Controllers/ProductController.cs
+++ DominosBackEnd/Dominos.WebUI/Controllers/ProductController.cs
@@ -11,7 +11,7 @@ namespace Dominos.WebUI.Controllers
 {
     public class ProductController : Controller
     {
-        OrderManager orderManager = new OrderManager(new EfOrderDal());
+        OrderManager _orderManager = new OrderManager(new EfOrderDal());
         ProductManager _productManager = new ProductManager(new EfProductDal());
         [HttpGet]
         public ViewResult Index()
@@ -33,10 +33,49 @@ namespace Dominos.WebUI.Controllers
             return View();
         }
 
-        public ActionResult SaveForm()
+        [HttpPost]
+        public JsonResult SaveForm(List<Parameter> parameters)
         {
 
-            return RedirectToAction("Index");
+            var itemList = new List<OrderItems>();
+            parameters.ForEach(p =>
+            {
+                for (int i = 0; i < Convert.ToInt32(p.Adet); i++)
+                {
+                    itemList.Add(new OrderItems
+                    {
+                        ProductId = Convert.ToInt32(p.Id),
+                        ProductPrice = Convert.ToDouble(p.Price),
+                        ProductDiscountPrice = Convert.ToDouble(p.DiscountPrice)
+                    });
+                }
+
+            });
+            var totalPrice = parameters.Where(s=> s.ProductTypeId != "3").Sum(s => Convert.ToDouble(s.Price) * Convert.ToInt32(s.Adet));
+
+            double discountPrice = totalPrice > 100 ? parameters.Where(s => s.ProductTypeId != "3").Sum(s => Convert.ToDouble(s.DiscountPrice)) : 0;
+
+            _orderManager.Add(new Order { CustomerAddressDesc = "", CustomerId = 1, TotalPrice = totalPrice, DiscountPrice = discountPrice }, itemList);
+            //Order ve OrderItems için Id yönetimi düzgün yapılmalı...
+            //Customer bilgileri alınıp servisle sorgulanıp eşlenen yoksa yeni id atılmalı...
+            //productType Tablosu kontrol edilmeli...
+            return Json(true);
         }
+
+
+    } 
+
+    public class Parameter
+    {
+        public string Id { get; set; }
+        public string Price { get; set; }
+        public string DiscountPrice { get; set; }
+        public string ProductTypeId { get; set; }
+        public string Adet { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Parameter> parameters { get; set; }
     }
 }
